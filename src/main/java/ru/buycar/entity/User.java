b104756d99294a1c252b931ea57adc8cda18ba46
MichaelZalogin <zaloginmiha@gmail.com>
2023-04-09 01:36:49 +0300

    HQL [#6874]

---
+++ src/main/java/ru/buycar/entity/User.java
@@ -0,0 +1,24 @@
+package ru.buycar.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString
+@Entity
+@Table(name = "auto_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String login;
+
+    private String password;
+}
