1119beaf177986cf4726fc1fd5ee023452b1ef9d
MichaelZalogin <zaloginmiha@gmail.com>
2023-04-13 01:27:09 +0300

    HQL [#6874]

--- src/main/java/ru/buycar/repository/UserRepository.java
+++ src/main/java/ru/buycar/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import ru.buycar.entity.User;
 
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +23,8 @@ public class UserRepository {
             session.getTransaction().commit();
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
         return user;
     }
@@ -43,6 +45,8 @@ public class UserRepository {
             session.getTransaction().commit();
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
     }
 
@@ -59,46 +63,81 @@ public class UserRepository {
             session.getTransaction().commit();
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
     }
 
     public List<User> findAllOrderById() {
         Session session = sf.openSession();
-        Query<User> query = session.createQuery("FROM User");
-        List<User> sortedList = query.list();
-        sortedList.sort(Comparator.comparingInt(User::getId));
-        return sortedList;
+        List<User> userList = new ArrayList<>();
+        try {
+            session.beginTransaction();
+            Query<User> query = session.createQuery("""
+                    FROM User
+                    ORDER BY id
+                    """);
+            userList = query.list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return userList;
     }
 
     public Optional<User> findById(int userId) {
         Session session = sf.openSession();
-        Query<User> query = session.createQuery("""
-                        FROM User
-                        WHERE id = :fId
-                        """)
-                .setParameter("fId", userId);
-        User user = query.uniqueResult();
-        return Optional.ofNullable(user);
+        Optional<User> userOptional = Optional.empty();
+        try {
+            Query<User> query = session.createQuery("""
+                            FROM User
+                            WHERE id = :fId
+                            """)
+                    .setParameter("fId", userId);
+            userOptional = query.uniqueResultOptional();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return userOptional;
     }
 
     public List<User> findByLikeLogin(String key) {
         Session session = sf.openSession();
-        Query<User> query = session.createQuery("""
-                        FROM User
-                        WHERE login LIKE CONCAT('%', :fKey, '%')
-                        """)
-                .setParameter("fKey", key);
-        return query.list();
+        List<User> userList = new ArrayList<>();
+        try {
+            Query<User> query = session.createQuery("""
+                            FROM User
+                            WHERE login LIKE :fKey
+                            """)
+                    .setParameter("fKey", "%" + key + "%");
+            userList = query.list();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return userList;
     }
 
     public Optional<User> findByLogin(String login) {
         Session session = sf.openSession();
-        Query<User> query = session.createQuery("""
-                        FROM User
-                        WHERE login LIKE :fLogin
-                        """)
-                .setParameter("fLogin", login);
-        User user = query.uniqueResult();
-        return Optional.ofNullable(user);
+        Optional<User> userOptional = Optional.empty();
+        try {
+            Query<User> query = session.createQuery("""
+                            FROM User
+                            WHERE login LIKE :fLogin
+                            """)
+                    .setParameter("fLogin", login);
+            userOptional = query.uniqueResultOptional();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return userOptional;
     }
 }
