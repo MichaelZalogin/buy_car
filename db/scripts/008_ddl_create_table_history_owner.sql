4d55e288513d3358138540f05baad72616feca8b
MichaelZalogin <zaloginmiha@gmail.com>
2023-05-18 23:40:06 +0300

    Машины и владельцы [#4744]

--- db/scripts/008_ddl_create_table_history_owner.sql
+++ db/scripts/008_ddl_create_table_history_owner.sql
@@ -3,5 +3,7 @@ CREATE TABLE history_owners
     id       BIGSERIAL PRIMARY KEY,
     car_id   BIGINT NOT NULL REFERENCES car (id),
     owner_id BIGINT NOT NULL REFERENCES owner (id),
+    start_at TIMESTAMP,
+    end_at   TIMESTAMP,
     UNIQUE (car_id, owner_id)
 );
