7169f3246aff5395ab0f5822f657f2f812a6e5cb
MichaelZalogin <zaloginmiha@gmail.com>
2023-05-30 23:49:52 +0300

    Репозитории [#325532]

--- src/main/java/ru/buycar/repository/hibernate/HibernateOwnerRepository.java
+++ src/main/java/ru/buycar/repository/hibernate/HibernateOwnerRepository.java
@@ -1,43 +1,56 @@
 package ru.buycar.repository.hibernate;
 
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
 import ru.buycar.entity.Owner;
 import ru.buycar.repository.BaseCrudRepository;
+import ru.buycar.repository.interfaces.OwnerRepository;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 @AllArgsConstructor
-public class HibernateOwnerRepository {
+@Repository
+public class HibernateOwnerRepository implements OwnerRepository {
 
     BaseCrudRepository baseCrudRepository;
 
-    public Owner create(Owner owner) {
+    @Override
+    public Owner addOwner(Owner owner) {
         baseCrudRepository.run(session -> session.persist(owner));
         return owner;
     }
 
-    public void update(Owner owner) {
+    @Override
+    public void updateOwner(Owner owner) {
         baseCrudRepository.run(session -> session.merge(owner));
     }
 
-    public void delete(Long ownerId) {
+    @Override
+    public void deleteOwner(Long ownerId) {
         baseCrudRepository.run(
                 "DELETE FROM Owner WHERE id = :fId",
                 Map.of("fId", ownerId)
         );
     }
 
-    public List<Owner> findAll() {
+    @Override
+    public List<Owner> findAllOwners() {
         return baseCrudRepository.query("FROM Owner", Owner.class);
     }
 
-    public Optional<Owner> findById(int ownerId) {
+    @Override
+    public Optional<Owner> findOwnerById(Long ownerId) {
         return baseCrudRepository.optional(
                 "FROM Owner WHERE id = :fId", Owner.class,
                 Map.of("fId", ownerId)
         );
+    }
+
+    @Override
+    public List<Owner> findAllOwnersByCarId(Long carId) {
+        return null;
     }
 
 }
--- src/main/java/ru/buycar/repository/interfaces/OwnerRepository.java
+++ src/main/java/ru/buycar/repository/interfaces/OwnerRepository.java
@@ -1,4 +1,22 @@
 package ru.buycar.repository.interfaces;
 
+import ru.buycar.entity.Owner;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface OwnerRepository {
+
+    Owner addOwner(Owner owner);
+
+    void updateOwner(Owner owner);
+
+    void deleteOwner(Long ownerId);
+
+    List<Owner> findAllOwners();
+
+    Optional<Owner> findOwnerById(Long ownerId);
+
+    List<Owner> findAllOwnersByCarId(Long carId);
+
 }
