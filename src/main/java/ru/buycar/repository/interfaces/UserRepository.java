13aef1e6fc9ebf79ab67fa17fa58740217fa0c42
MichaelZalogin <zaloginmiha@gmail.com>
2023-06-20 23:43:45 +0300

    Add service layer [#325532]

--- src/main/java/ru/buycar/repository/interfaces/UserRepository.java
+++ src/main/java/ru/buycar/repository/interfaces/UserRepository.java
@@ -7,12 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository {
 
-    User addUser(User user);
-
-    void updateUser(User user);
-
-    void deleteUser(Long userId);
-
     List<User> findAllUsersOrderById();
 
     Optional<User> findUserById(Long userId);
